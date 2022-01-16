83d2899eae53cbfd4b208cc0c4e7a84ea448a5e3
HXY <1969574105@qq.com>
2022-01-16 14:10:25 +0800

    👑：添加了第一个自己基于axios封装的http请求方法httpReq

---
+++ httpReq.js
@@ -0,0 +1,53 @@
+/* 
+  自定义封装的http请求方法——httpReq
+  其中对错误进行了统一处理
+*/
+import axios from 'axios'
+import { message } from 'antd'
+// 给请求地址前加一个"/api"，在请求头中添加Token
+const instance = axios.create({
+  baseURL: '/',
+})
+// 添加返回拦截器，直接获取返回内容的data
+instance.interceptors.response.use((res) => {
+  return res.data
+})
+// 封装axios方法
+export const httpReq = (method, url, data) => {
+  return new Promise((resolve, reject) => {
+    instance({
+      method: method,
+      url: url,
+      data: data,
+    }).then(
+      (data) => {
+        resolve(data)
+      },
+      (err) => {
+        // 错误在这统一处理
+        const status = err.response.status
+        const errInfo = err.response.data.message || status
+        // 根据状态码做提示处理
+        switch (status) {
+          case 401:
+            message.error(`认证失败: ${errInfo}`)
+            break
+          case 403:
+            message.error(`未授权: ${errInfo}`)
+            setTimeout(() => {
+              window.location.href = '/'
+            }, 1500)
+            break
+          case 404:
+            message.error(`未找到资源: ${errInfo}`)
+            break
+          case 500:
+            message.warning(`服务器未能处理: ${errInfo}`)
+            break
+          default:
+            break
+        }
+      }
+    )
+  })
+}
